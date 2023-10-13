b48c328abb62d64ad9b6f25967c40c409f3e77ae
Coconut <coconutat@protonmail.com>
2023-10-13 20:08:20 +0800

    Update Build Scripts and clean script

--- Build_kernel.sh
+++ Build_kernel.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+#设置环境
 
-rm -r out
+# Special Clean For Huawei Kernel.
+if [ -d include/config ];
+then
+    echo "Find config,will remove it"
+	rm -rf include/config
+else
+	echo "No Config,good."
+fi
+
 
 echo " "
 echo "***Setting environment...***"
 # 交叉编译器路径
-#export PATH=$PATH:$(pwd)/toolchain/linux-x86_64/bin
-#export CROSS_COMPILE=aarch64-linux-android-
-
-export PATH=$PATH:/home/abdelhy/Desktop/P20_lite_Kernels/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-elf/bin
-export CROSS_COMPILE=aarch64-elf-
-
-#export PATH=$PATH:/home/abdelhy/Desktop/P20_lite_Kernels/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin
-#export CROSS_COMPILE=aarch64-elf-
+export PATH=$PATH:$(pwd)/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
 
 export GCC_COLORS=auto
 export ARCH=arm64
@@ -21,22 +24,83 @@ then
 	mkdir out
 fi
 
-date="$(date +%d%m%Y-%I%M)"
+start_time=$(date +%Y.%m.%d-%I_%M)
 
-#sync with kerenSU
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+start_time_sum=$(date +%s)
 
-cp sepolicy.c $(pwd)/KernelSU/kernel/selinux/
+#!/bin/bash
 
-echo "***Building for V9 version...***"
-make ARCH=arm64 O=out merge_kirin710_defconfig
-make ARCH=arm64 O=out -j8  2>&1 | tee log-${date}.log
+# 提示用户选择选项
+echo "请选择一个选项："
+echo "1. 设置defconfig参数为Nova4e"
+echo "2. 设置defconfig参数为荣耀8X"
+echo "3. 设置defconfig参数为默认710配置"
+echo "请输入数字1到3"
+
+# 读取用户输入的选项
+read choice
+
+# 根据用户选择设置defconfig参数
+case $choice in
+  1)
+    defconfig="Nova4e_Kirin710_KSU_defconfig"
+    ;;
+  2)
+    defconfig="JSN_kirin710_KSU_defconfig"
+    ;;
+  3)
+    defconfig="kirin710_KSU_defconfig"
+    ;;
+  *)
+    echo "无效的选项"
+    exit 1
+    ;;
+esac
+
+# 输出所选的defconfig参数
+echo "已选择的defconfig参数为: $defconfig"
 
 
-echo "***Packing V9 version kernel...***"
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00078000 --cmdline "oglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --base 0x00078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --second_offset 0x00e88000 --tags_offset 0x07988000 --os_version 9.0.0 --os_patch_level 2020-09 --header_version 1 --output Kernel-4.9.148-pot-KSU-enforcing-${date}.img
+#构建内核部分
+echo "***Building kernel...***"
+make ARCH=arm64 O=out ${defconfig}
+# 定义编译线程数
+make ARCH=arm64 O=out -j64 2>&1 | tee kernel_log-${start_time}.txt
 
+end_time_sum=$(date +%s)
 
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --base 0x00000100 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --os_version 9.0.0 --os_patch_level 2020-01 --header_version 0 --output Kernel-4.9.148-pot-KSU-permissive-${date}.img
+end_time=$(date +%Y.%m.%d-%I_%M)
+
+# 计算运行时间（秒）
+duration=$((end_time_sum - start_time_sum))
+
+# 将秒数转化为 "小时:分钟:秒" 形式输出
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
+
+# 打印运行时间
+echo "脚本运行时间为：${hours}小时 ${minutes}分钟 ${seconds}秒"
+
+#打包内核
+
+if [ -f out/arch/arm64/boot/Image.gz ];
+then
+	echo "***Packing kernel...***"
+
+	cp out/arch/arm64/boot/Image.gz Image.gz 
 	
+	# Pack Enforcing Kernel
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-05-05 --output Kirin710_EMUI_9.1.0-${end_time}.img
+	
+	# Pack Permissive Kernel
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-05-05 --output Kirin710_EMUI_9.1.0_PM-${end_time}.img
 
+	echo "***Sucessfully built kernel...***"
+	echo " "
+	exit 0
+else
+	echo " "
+	echo "***Failed!***"
+	exit 0
+fi
